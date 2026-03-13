318d08120fa96bdee8898fc1c4cd3f3c237d4366
piluga <133677361+piluga@users.noreply.github.com>
2026-03-13 20:03:43 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'cruscotto-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json'
+];
+
+// Salva i file grafici durante l'installazione
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Fornisce i file dalla cache per un caricamento lampo
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
